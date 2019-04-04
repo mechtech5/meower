e6f8d6da580b561df22021f8a67918fb80d407ea
Ayush Likhar <neontechhead@gmail.com>
2019-04-04 09:29:12 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
+
+const app = express();
+const db = monk('localhost/meower'); //database
+const mews = db.get('mews'); //collection
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Meower!'
+  });
+});
+
+app.get('/mews', (req, res) => {
+  mews
+    .find()
+    .then(mews => {
+      res.json(mews);
+    })
+});
+
+function isValidMew(mew) {
+  return mew.name && mew.name.toString().trim() !== '' &&
+    mew.content && mew.content.toString().trim() !== '';
+}
+
+app.post('/mews', (req, res) => {
+  if(isValidMew(req.body)) {
+    //insert in db...
+    const mew = {
+      name: req.body.name.toString(),
+      content: req.body.content.toString(),
+      created: new Date()
+    }
+    
+    mews
+      .insert(mew)
+      .then(createdMew => {
+        res.json(createdMew);
+      });
+  } else {
+    res.status(422);
+    res.json({
+      message: 'Hey! Name and Content are required'
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Listening on http://localhost:5000');
+});
